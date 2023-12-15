35481e563bc64287c4b2117587b9f3fe7733bfbe
BilalSamiMirza <bilalsamimirza@gmail.com>
2023-12-16 02:43:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,316 @@
+// var real =(prompt ("First Name"));
+// var real =(prompt ("Last Name"));
+// var real =(prompt ("Email"));
+// var real =(prompt ("Phone Number"));
+// var real =(prompt ("Passwords"));
+// alert("You're learning JavaScript!");
+// var message ="Thankyou for your information";
+// alert (message);
+
+
+// var myVariable;
+// var secondVariable;
+// secondVariable = "Hello, I'm a string!";
+
+// var teamName = "Team Awesome";
+// alert(teamName);
+
+
+// var bestMan = "Bilal";
+// bestMan = "Sami"; 
+
+// var myVariable;
+
+
+// var secondVariable;
+// secondVariable = "Hello, I'm a string!";
+
+// var teamName = "Team Awesome";
+// alert(teamName);
+
+
+// var bestMan = "Bilal";
+// bestMan = "Sami"; 
+
+// var caseQty;
+// caseQty=144;
+// var num = 9;
+// var sumOfNumbers;
+// sumOfNumbers = 5 + 7; 
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert("value of order total:\n" + orderTotal)
+
+// var myNumber = 10;
+// AddedNumber = myNumber+5;
+
+// var productCost = 3.45;
+
+// var nameofBand;
+// var legalVariable = 42 ;
+
+// var fullName = "FirstNameLastName";
+// var  legalVariable;
+// var  anotherLegalVariable;
+// var  _underscoreVariable;
+// var  $dollarSignVariable;
+// var  camelCaseVariable;
+
+// var 123illegalVariable; // Starts with a number
+// var illegal-variable;   // Contains a hyphen
+// var illegal variable;    // Contains a space
+// var illegal&Variable;    // Contains an ampersand
+
+
+// console.log("Q.What is the namee and symbol of the arithmetic operator that gives you the remainder when one number is divided by another \n ans.%")
+// // Task 3
+// var largeNum = 1000 * 2000;
+
+// // Task 4
+// var firstVariable = 500; 
+// var secondVariable = 200; 
+// var difference = firstVariable - secondVariable;
+
+// // Task 5
+// var dividend = 10; 
+// var divisor = 3; 
+// var remainder = dividend % divisor;
+
+// // Task 6
+// var num1 = 8; 
+// var num2 = 5; 
+// var multiplicationResult = num1 * num2;
+// alert("The result of the multiplication is: " + multiplicationResult);
+// x += 1; 
+// x -= 1;
+// var newNum = --num;
+// var originalVar = 5; 
+// var anotherVar = originalVar++;
+// var number = 10; 
+// number++;
+// alert("The new value is: " + number);
+
+
+// // 1. var num = "2" + "2";
+// // The value of num would be "22" (including the quotation marks) because when two strings are added together using the + operator in JavaScript, they are concatenated, not numerically added.
+
+// // 2. message = ("Hello," + "Dolly");
+// // The value of message would be "Hello,Dolly" (including the quotation marks) because the + operator concatenates the two strings together.
+
+// // 3. alert("33" + 3);
+// // The message displayed in the alert box would be "333" (including the quotation marks) because the number 3 gets coerced into a string due to concatenation with the string "33".
+// alert("Pakistan " + "Zindabad");
+// var concatenated = "Number: " + 10;
+// // This would assign the value "Number: 10" to the variable concatenated
+// var firstString = "Hello";
+// var secondString = "World";
+// var concatenatedString = firstString + secondString;
+
+// if (variable1 !== variable2) {
+// }
+// if (variable1 >= variable2) {
+// }
+// var variable = 10; 
+// var particularNumber = 5; 
+
+// if (variable !== particularNumber) {
+//     variable = 15;
+// }
+// var number1 = 10;
+// var number2 = 20;
+
+// if (number1 !== number2) {
+//     alert("Congratulations!");
+// }
+// var expectedName = "John"; 
+// var userInput = prompt("Enter your first name:");
+
+// if (userInput !== expectedName) {
+//     alert("No match");
+// }
+// if (variable1 !== variable2) {
+// }
+// var marks = parseFloat(prompt("Enter your marks:"));
+
+// if (marks >= 90) {
+//     alert("Your grade is A");
+// } else if (marks >= 60) {
+//     alert("Your grade is B");
+// } else if (marks >= 40) {
+//     alert("Your grade is C");
+// } else {
+//     alert("Your grade is F");
+// }
+// if (a === 10) {
+//     alert("a is 10");
+// } else {
+//     alert("The correct value of a is ____");
+// }
+// var city = prompt("Enter a city:");
+
+// if (city === "Karachi") {
+//     alert("Acknowledging it is Karachi");
+// } else if (city === "Lahore") {
+//     alert("Acknowledging it's Lahore");
+// } else {
+//     alert("Different city");
+// }
+// if (a === b && c === d) {
+// }
+// if (a === b || c !== d) {
+// }
+// if ((name === "Hamza" || name === "Arsalan") && age < 60) {
+// }
+// var firstVar = 10;
+// var secondVar = 20;
+
+// if (firstVar < secondVar || firstVar > secondVar) {
+//     alert("Condition met");
+// }
+// var firstName = "John"; 
+// var lastName = "Doe"; 
+// var userInputFirstName = prompt("Enter your first name:");
+// var userInputLastName = prompt("Enter your last name:");
+
+// if (userInputFirstName === firstName && userInputLastName === lastName) {
+//     alert("Names match!");
+// }
+// var password = "examplePassword"; 
+
+// if (password !== "") {
+//     if (password.length <= 5) {
+//         alert("Password must be greater than 5");
+//     } else {
+//         alert("OK");
+//     }
+// }
+// if (a === 1) {
+//     if (c === "Max") {
+//         alert("OK");
+//     }
+// }
+// if (a === 1 && c === "Max") {
+//     alert("OK");
+// }
+// var var1 = 10;
+// var var2 = 10;
+
+// if (var1 === var2) {
+//     if (var1 <= var2) {
+//         alert("Conditions met");
+//     }
+// }
+// var emptyArray = [];
+// var arrayWithString = ["Hello"];
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]); 
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var totalLength = alphabet.length;
+// var newArray = ["First element"];
+// newArray[1] = "Second element";
+// alert(newArray[1]); 
+// var array = ["Element 1"];
+// array.push("Element 2");
+// alert(array[array.length - 1]); // Displaying the last element added
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var totalLength = alphabet.length;
+
+// var array = ["Element 1"];
+// array.push("Element 2");
+// alert(array[array.length - 1]); 
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.pop();
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(10);
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift(1, 2, 3); 
+// var array = ["First element"];
+// array.unshift("New first element");
+// alert(array[0]); 
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2, 0, "L");
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var regSizes = sizes.slice(0, 3); 
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 3, "elephant", "horse"); 
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 2);
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// var reducedArray = pets.slice(3, 5); 
+
+
+// for (var i = 0; i < 10; i++) {
+    
+// }
+// for (var i = 0; i <= 11; i++) {
+    
+// }
+// for (var j = 0; j < 100; j++) {
+    
+// }
+// for (var k = 3; k > 0; k--) {
+    
+// }
+// var arrayLength = array.length;
+// var flag = true; 
+// for (var m = 0; m < pets.length; m++) {
+    
+// }
+// for (var n = 0; n < 10; n++) {
+//     if (n === 1) {
+//         alert("Counter is: " + n);
+//         break;
+//     }
+// }
+// var userNames = ["Alice", "Bob", "Charlie"]; 
+
+// var firstName = prompt("Enter first name:");
+
+// for (var p = 0; p < userNames.length; p++) {
+//     if (firstName === userNames[p]) {
+//         alert("Enter");
+//         break;
+//     } else {
+//         alert("Please write correct user name");
+//         break;
+//     }
+// }
+// var matchFound = false;
+// for (var q = 0; q < list.length; q++) {
+//     if (userInput === list[q]) {
+//         alert("Match found");
+//         matchFound = true;
+//         break;
+//     }
+// }
+// if (!matchFound) {
+//     alert("Match not found");
+// }
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var r = 0; r < firstArr.length; r++) {
+//     for (var s = 0; s < secondArr.length; s++) {
+//         console.log(firstArr[r] + secondArr[s]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
